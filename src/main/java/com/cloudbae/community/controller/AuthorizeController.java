d443e7bf94297930684d554b5b17c256e47fd262
YandaS <songyandasyd@gmail.com>
2019-09-23 18:28:26 +0800

    得到 Github 用户名

--- src/main/java/com/cloudbae/community/controller/AuthorizeController.java
+++ src/main/java/com/cloudbae/community/controller/AuthorizeController.java
@@ -1,4 +1,31 @@
 package com.cloudbae.community.controller;
 
-public class Authorize {
+import com.cloudbae.community.dto.AccessTokenDTO;
+import com.cloudbae.community.dto.GithubUser;
+import com.cloudbae.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("5a6348fb4b510ec429ea");
+        accessTokenDTO.setClient_secret("b543d482569413c627912ff43cea48adb99136c4");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.githubUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
 }
