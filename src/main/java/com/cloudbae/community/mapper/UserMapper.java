0647d4bafb790d223908c043e3374d9ab6073b56
YandaS <songyandasyd@gmail.com>
2019-09-24 15:28:16 +0800

    add check cookies

--- src/main/java/com/cloudbae/community/mapper/UserMapper.java
+++ src/main/java/com/cloudbae/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.cloudbae.community.mapper;
 import com.cloudbae.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -11,7 +13,8 @@ public interface UserMapper {
     @Insert("INSERT user (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
-
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
 
 
 }
