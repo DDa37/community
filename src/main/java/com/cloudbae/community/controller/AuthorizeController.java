224530986c79ac48507ac5eb5bce7651bb1ffc58
YandaS <songyandasyd@gmail.com>
2019-09-24 14:47:54 +0800

    写入 MySQL数据库

--- src/main/java/com/cloudbae/community/controller/AuthorizeController.java
+++ src/main/java/com/cloudbae/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.cloudbae.community.controller;
 
 import com.cloudbae.community.dto.AccessTokenDTO;
 import com.cloudbae.community.dto.GithubUser;
+import com.cloudbae.community.mapper.UserMapper;
+import com.cloudbae.community.model.User;
 import com.cloudbae.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -27,6 +29,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -39,10 +44,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.githubUser(accessToken);
-        if(user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功。写sseison 和 cookie
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else {
             //登录失败，重新登录
--- src/main/java/com/cloudbae/community/mapper/UserMapper.java
+++ src/main/java/com/cloudbae/community/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package com.cloudbae.community.mapper;
 
-public class UserMapper {
+import com.cloudbae.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("INSERT user (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+
+
+
 }
