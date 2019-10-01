d83af661d3d4b2ae70b25ae5e70a0b59021c214b
YandaS <songyandasyd@gmail.com>
2019-10-01 21:36:08 +0800

    增加 根据id获取数据的SQL

--- src/main/java/com/cloudbae/community/mapper/UserMapper.java
+++ src/main/java/com/cloudbae/community/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Mapper
@@ -16,5 +17,6 @@ public interface UserMapper {
     @Select("SELECT * FROM user WHERE token = #{token}")
     User findByToken(@Param("token") String token);
 
-
+    @Select("SELECT * FROM user WHERE id = #{id}")
+    User findById(@Param("id")Integer id);
 }
