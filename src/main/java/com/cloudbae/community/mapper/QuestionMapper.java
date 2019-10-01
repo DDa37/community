e3244a5568d66c5e6fd846a93e4ec963645defd8
YandaS <songyandasyd@gmail.com>
2019-10-01 21:37:15 +0800

    no message

--- src/main/java/com/cloudbae/community/mapper/QuestionMapper.java
+++ src/main/java/com/cloudbae/community/mapper/QuestionMapper.java
@@ -3,12 +3,18 @@ package com.cloudbae.community.mapper;
 import com.cloudbae.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
 
     @Insert("INSERT INTO question (title,description,gmt_create,gmt_modified,creator,tag) VALUES (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
      void create(Question question);
+
+    @Select("SELECT * FROM question")
+    List<Question> list();
 
 }
 
