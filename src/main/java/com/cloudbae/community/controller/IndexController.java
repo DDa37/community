460bee927819bd53d4d8ee29447dad3047d5612e
YandaS <songyandasyd@gmail.com>
2019-10-01 21:35:04 +0800

    在跳转页面前，先获取question数据

--- src/main/java/com/cloudbae/community/controller/IndexController.java
+++ src/main/java/com/cloudbae/community/controller/IndexController.java
@@ -1,13 +1,17 @@
 package com.cloudbae.community.controller;
 
+import com.cloudbae.community.dto.QuestionDTO;
 import com.cloudbae.community.mapper.UserMapper;
 import com.cloudbae.community.model.User;
+import com.cloudbae.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Controller
@@ -16,8 +20,12 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public  String index(HttpServletRequest request){
+    public  String index(HttpServletRequest request,
+                         Model model){
         Cookie [] cookies = request.getCookies();
         if (cookies != null && cookies.length !=0) {
             for (Cookie cookie : cookies) {
@@ -31,6 +39,10 @@ public class IndexController {
                 }
             }
         }
+
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
