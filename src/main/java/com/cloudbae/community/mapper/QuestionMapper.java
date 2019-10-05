d128d92d43869996b512e44d30d44dd34b7c9959
YandaS <songyandasyd@gmail.com>
2019-10-05 14:45:05 +0800

    no message

--- src/main/java/com/cloudbae/community/mapper/QuestionMapper.java
+++ src/main/java/com/cloudbae/community/mapper/QuestionMapper.java
@@ -3,6 +3,7 @@ package com.cloudbae.community.mapper;
 import com.cloudbae.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -13,8 +14,10 @@ public interface QuestionMapper {
     @Insert("INSERT INTO question (title,description,gmt_create,gmt_modified,creator,tag) VALUES (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
      void create(Question question);
 
-    @Select("SELECT * FROM question")
-    List<Question> list();
+    @Select("SELECT * FROM question LIMIT #{offset},#{size}")
+    List<Question> list(@Param(value = "offset") Integer offset,@Param(value = "size") Integer size);
 
+    @Select("SELECT COUNT(1) FROM question")
+    Integer count();
 }
 
