a2f3dcb44374b4b6c6bf1bac8e5486bcfc485132
YandaS <songyandasyd@gmail.com>
2019-10-19 01:05:48 +0800

    增加一个拦截器

--- src/main/java/com/cloudbae/community/controller/IndexController.java
+++ src/main/java/com/cloudbae/community/controller/IndexController.java
@@ -1,8 +1,6 @@
 package com.cloudbae.community.controller;
 
 import com.cloudbae.community.dto.PaginationDTO;
-import com.cloudbae.community.mapper.UserMapper;
-import com.cloudbae.community.model.User;
 import com.cloudbae.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,15 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
+
 import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
@@ -28,19 +23,6 @@ public class IndexController {
                          Model model,
                          @RequestParam(name = "page",defaultValue = "1") Integer page,
                          @RequestParam(name = "size",defaultValue = "5") Integer size){
-        Cookie [] cookies = request.getCookies();
-        if (cookies != null && cookies.length !=0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
         PaginationDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination",pagination);
         return "index";
