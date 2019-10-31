30f616b4fe3a27164ea634bee163c01599f7ec0e
YandaS <songyandasyd@gmail.com>
2019-10-31 14:26:19 +0800

    add CommentController

--- src/main/java/com/my/community/controller/CommentController.java
+++ src/main/java/com/my/community/controller/CommentController.java
@@ -1,4 +1,30 @@
 package com.my.community.controller;
 
+import com.my.community.dto.CommentDTO;
+import com.my.community.mapper.CommentMapper;
+import com.my.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object psot(@RequestBody CommentDTO commentDTO){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        return null;
+    }
 }
